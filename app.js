7590874a4e3a18d2c9c20e1e865372c54978e7af
Rbb621 <Rbberliant@gmail.com>
2022-10-28 14:49:20 -0500

    three new staff

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 'use strict';
 
 //**********************************
+//     FAQ + and - Signs
+//**********************************
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+//**********************************
 //     Circle Images Specialties
 //**********************************
 
@@ -38,7 +62,7 @@ document.getElementsByClassName("thankYou").value = `Thank you for signup Better
 
 
 
-$('.Staff').click(function() {
+$('.Staff').hover(function() {
   $(".dropdown-menu.Staffdropdown").toggle();})
 
 $('.specialties-heading').hover(function() {
